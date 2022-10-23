42f5d1a064beb81bfbc6d8207d599d3926ab7242
Kenneth Cruz <kenneth-cruz@outlook.com>
2022-10-23 07:57:49 -0600

    Pantallas modales y fucionalidad del localstorage

--- js/controlador.js
+++ js/controlador.js
@@ -27,7 +27,7 @@ function getCategories(){
 }
 
 
-function updateDropdownMenu(username){
+function updateDropdownMenu(){
 
     const dropdownMenuUsers = document.getElementById('dropdownMenuUsers');
     dropdownMenuUsers.innerHTML = '';
@@ -40,20 +40,26 @@ function updateDropdownMenu(username){
     });
 }
 
+var username;
 
 function loadUsernameDropdown(username){
+    
+    this.username = username;
+    const btnSeeOrders = document.getElementById('seeOrders');
+    btnSeeOrders.disabled = false;
+
     const btnDropDownUser = document.getElementById('dropdownUsers');
     btnDropDownUser.innerHTML = '';
     btnDropDownUser.innerHTML = `${username}`;
 
-    updateDropdownMenu(username);
-    greeting(username.split(' ')[0]);
+    updateDropdownMenu();
+    greeting();
 }
 
 
 function populateDropdownMenuUsers(){
     
-    let userDB = getUsers();
+    let userDB = getUsers(); 
     const dropdownMenuUsers = document.getElementById('dropdownMenuUsers');
 
     userDB.forEach(user => {
@@ -90,6 +96,7 @@ function populateDataCardCategory(){
 
 
 function populateDataModalCategory(nameCategory){
+
     let categoriesDB = getCategories();
     let category = (categoriesDB.filter( category => category.nombreCategoria === nameCategory ))[0];
     const categoryDataModalContent = document.getElementById('categoryDataModalContent');
@@ -110,11 +117,9 @@ function populateDataModalCategory(nameCategory){
 
         </div>
         <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-            <button type="button" class="btn btn-primary">Save changes</button>
         </div>
     `;
-    console.log(category);
+    
 }
 
 
@@ -126,13 +131,15 @@ function populateDataCardCompany(companies){
             `
             <div class="col-5 mt-4 ml-4 mb-4">
                 <div class="card" style="width:20rem;">
-                    <img src="${company.imagen}" class="card-img-top" alt="...">
+                    <div>
+                        <img src="${company.imagen}" class="card-img-top" alt="..." style="position: relative;">
+                        <div class="ml-4 logo-title" style="position: absolute; font-size: 1.8em; top: 26%;">${company.nombreEmpresa}</div>
+                    </div>
                     ${populateListGroupItemProduct(company.productos)}
                 </div>
             </div>
             `
             )
-            console.log( company.productos )
     });
     
     return companiesList.join(' ');
@@ -142,6 +149,9 @@ function populateDataCardCompany(companies){
 function populateListGroupItemProduct(products){
     
     let productList = []
+
+    const btnSeeOrders = document.getElementById('seeOrders');
+
     products.forEach(product => {
         productList.push(
             `
@@ -154,7 +164,9 @@ function populateListGroupItemProduct(products){
                                 </div>
 
                                 <div class="col-5">
-                                    <button class="btn paragraph"  style="background-color: #440f71; color: white; font-size: 0.8rem; border-radius: 1.2rem;">
+                                    <button 
+                                            class="btn paragraph"  
+                                            style="background-color: #440f71; color: white; font-size: 0.8rem; border-radius: 1.2rem;" ${btnSeeOrders.disabled? 'disabled' : ''} data-toggle="modal" data-target="#processOrderDataModal" onClick="populateFormProcessOrder('${product.nombreProducto}', '${product.descripcion}', ${product.precio})">
                                         Pedir
                                     </button>
                                 </div>
@@ -183,16 +195,75 @@ function populateListGroupItemProduct(products){
             `
         )
     });
-    
+
     return productList.join(' ');
 }
 
 
-function greeting(username){
+function activateModal(id){
+    $(`#${id}`).modal({show:true});
+}
+
+
+function populateFormProcessOrder(productName, productDescription, priceProduct){
+    
+    const modalContent = document.getElementById('processOrderModalContent'); 
+    modalContent.innerHTML = `
+    <div class="modal-header">
+        <h5 class="modal-title paragraph" style="color: #5c2e87;">${productName}</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+    </div>
+    <div class="modal-body">
+
+        <div class="container">
+            <div class="row">
+                <div class="col-8 paragraph" style="color: #5d5d5d; font-size: 0.85rem;">
+                    ${productDescription.substr(0, 27)}
+                </div>
+
+                <div class="col-4">
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-7 paragraph">
+                    Cantidad a solicitar: 
+                </div>
+
+                <div class="col-5 paragraph" >
+                    <input class="form-control" type="number">
+                </div>
+            </div>
+            <br>
+            <div class="row">
+                <div class="col-4 paragraph" style="color: #5d5d5d; font-weight: 900; text-align:right;">
+                </div>
+                <div class="col-8 paragraph" style="color: #5d5d5d; font-weight: 900; text-align:right;">
+                    ${formatCurrency(priceProduct)}
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+    <div class="modal-footer">
+        <button 
+            class="btn paragraph"  
+            style="background-color: #440f71; color: white; font-size: 0.8rem; border-radius: 1.2rem;">
+            Procesar Orden
+        </button>
+    </div>
+    `
+}
+
+
+function greeting(){
 
     const bodyGreeting = document.getElementById('greeting');
     bodyGreeting.innerHTML = '';
-    bodyGreeting.innerHTML += `<div class="title body-greeting">¡Hola ${username}!</div>`;
+    bodyGreeting.innerHTML += `<div id="bodyGreeting" class="title body-greeting">¡Hola ${username.split(' ')[0]}!</div>`;
     bodyGreeting.innerHTML += `<p class="paragraph body-greeting">¿Qué necesitas?</p>`;
 }
 
